5778b9c1a58b0f305c115508232810ec62bfd020
Arturo Castro <armicasdi@gmail.com>
2019-09-25 16:31:27 -0600

    added Color to BG

--- app/src/main/java/com/example/funfacts/MainActivity.java
+++ app/src/main/java/com/example/funfacts/MainActivity.java
@@ -2,19 +2,27 @@ package com.example.funfacts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+
     //Declare our View Variables
 
     private TextView FactTextView;
     private Button ShowFactButton;
+
+    private RelativeLayout relativeLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +32,20 @@ public class MainActivity extends AppCompatActivity {
         //Assign The Views form the layout file to the corresponding variables
         FactTextView = findViewById(R.id.factTextView);
         ShowFactButton = findViewById(R.id.showFactButton);
+
+        relativeLayout = findViewById(R.id.relativeLayout);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built.",
-                        "Treehouse is not actually in a tree"};
-
-                    //The button was click so Update the fact text view
-                //Randomly Select a Fact
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                String fact = facts[randomNumber];
+                String fact = factBook.getFact();
 
                 //Update the screen with our new fact
                 FactTextView.setText(fact);
+                int color = colorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                ShowFactButton.setTextColor(color);
+
             }
         };
         ShowFactButton.setOnClickListener(listener);
